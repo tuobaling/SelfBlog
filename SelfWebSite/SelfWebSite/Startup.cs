6ca0891d51a8464f71d4cc0a59431e98c67f0cd1
tuobaling <wunhueic+hub@gmail.com>
2015-08-18 15:59:51 +0800

    Ver2
    Testing

---
+++ SelfWebSite/SelfWebSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SelfWebSite.Startup))]
+namespace SelfWebSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
